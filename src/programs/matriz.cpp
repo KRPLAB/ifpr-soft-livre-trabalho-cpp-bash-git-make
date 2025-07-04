1cac9f5ad23498bdecbfb6068f4286daccaeccb2
KamilliBach <189908551+KamilliBach@users.noreply.github.com>
2025-07-03 21:48:10 -0300

    refatoração de código no programa matriz.cpp

--- src/programs/matriz.cpp
+++ src/programs/matriz.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
-#include <vector>  
+
+#define MAX_D 1000
+
+using namespace std;
 
 int main() {
-    int linhas, colunas;
+    int matriz[MAX_D][MAX_D];
+    int n;
 
-    std::cout << " n linhas: ";
-    std::cin >> linhas;
-    std::cout << "n de colunas: ";
-    std::cin >> colunas;
+	cout << "Dimensoes da matriz:\n";
+	cin >> n;
 
-    
-    if (linhas <= 0 || colunas <= 0) {
-        std::cout << "tem que ser maior que 0\n"; 
-        return 1; 
-    }
+	if (n <= 0 || n > MAX_D) {
+		cout << "Fora das dimensoes permitidas\n";
+		return 1;
+	}
 
-    std::vector<std::vector<int>> matriz(linhas, std::vector<int>(colunas));
+	
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cin >> matriz[i][j];
+		}
+	}
 
-    for (int i = 0; i < linhas; ++i) {
-        for (int j = 0; j < colunas; ++j) {
-            std::cout << "elemento [" << i << "][" << j << "]: ";
-            std::cin >> matriz[i][j];
-        }
-    }
+	cout << "Matriz\n";
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cout << matriz[i][j] << "\t";
+		}
+		cout << endl;
+	}
 
-    std::cout << " Matriz\n";
-    for (int i = 0; i < linhas; ++i) {
-        for (int j = 0; j < colunas; ++j) {
-            
-            std::cout << matriz[i][j] << "\t"; 
-        }
-        std::cout << std::endl; 
-    }
-
-    return 0; 
+	return 0;
 }
