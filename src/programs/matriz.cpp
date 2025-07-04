dfd6ec0c898800ca90c58e598898776b4ca8bfd2
KamilliBach <189908551+KamilliBach@users.noreply.github.com>
2025-07-03 21:52:47 -0300

    modularização do programa matriz.cpp

--- src/programs/matriz.cpp
+++ src/programs/matriz.cpp
@@ -4,32 +4,38 @@
 
 using namespace std;
 
-int main() {
-    int matriz[MAX_D][MAX_D];
-    int n;
+void leitura_matriz(int m[MAX_D][MAX_D], int tam){
+	for (int i = 0; i < tam; ++i){
+		for (int j = 0; j < tam; ++j) {
+			cin >> m[i][j];
+		}
+	}
+}
+
+void impressao_matriz(int m[MAX_D][MAX_D], int tam){
+	cout << "Matriz\n";
+	for (int i = 0; i < tam; ++i){
+		for (int j = 0; j < tam; ++j){
+			cout << m[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+	int matriz[MAX_D][MAX_D];
+	int n;
 
 	cout << "Dimensoes da matriz:\n";
 	cin >> n;
 
-	if (n <= 0 || n > MAX_D) {
+	if (n <= 0 || n > MAX_D){
 		cout << "Fora das dimensoes permitidas\n";
 		return 1;
 	}
 
-	
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			cin >> matriz[i][j];
-		}
-	}
-
-	cout << "Matriz\n";
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			cout << matriz[i][j] << "\t";
-		}
-		cout << endl;
-	}
+	leitura_matriz(matriz, n);
+	impressao_matriz(matriz, n);
 
 	return 0;
 }
