88aec12bc5a8a22107702ed982eb887c6c743654
KRPLAB <kauan.r.paulino@gmail.com>
2025-07-04 10:37:50 -0300

    adição de arquivo de saída no script

--- tests/generate-test-primo.sh
+++ tests/generate-test-primo.sh
@@ -5,6 +5,7 @@ TEST_DIR="./primo"
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 INPUT_FILE="$TEST_DIR/test-$TIMESTAMP-primo.in"
+OUTPUT_FILE="$TEST_DIR/test-$TIMESTAMP-primo.out"
 
 RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
 
@@ -13,4 +14,4 @@ echo "$RANDOM_NUMBER" > "$INPUT_FILE"
 echo "Arquivo de teste para primo.cpp gerado em: $INPUT_FILE"
 echo "Conteúdo: $RANDOM_NUMBER"
 
-../bin/primo < "$INPUT_FILE"
+../bin/primo < "$INPUT_FILE" >> "$OUTPUT_FILE"
