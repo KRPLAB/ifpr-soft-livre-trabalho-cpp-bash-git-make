7e7ce52f1af4c2f020a46f20d0f54eb9d744bef4
KRPLAB <kauan.r.paulino@gmail.com>
2025-07-04 10:31:29 -0300

    script para gerar primo criado

---
+++ tests/generate-test-primo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEST_DIR="./tests/primo"
+mkdir -p "$TEST_DIR"
+
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+INPUT_FILE="$TEST_DIR/test-$TIMESTAMP-primo.in"
+
+RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+echo "$RANDOM_NUMBER" > "$INPUT_FILE"
+
+echo "Arquivo de teste para primo.cpp gerado em: $INPUT_FILE"
+echo "Conteúdo: $RANDOM_NUMBER"
+
+../bin/primo < "$INPUT_FILE"
